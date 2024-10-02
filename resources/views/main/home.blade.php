095a97a2fabd8aad9bb058d09c92946e9521190e
LinKuangren <romaindanne@gmail.com>
2024-10-03 01:05:44 +0200

    home page

---
+++ resources/views/main/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.base')
+
+@section('content')
+    <h1 class="text-4xl font-bold text-center">Qui je suis ?</h1>
+    <p>zejhf ezlkfh ezkjf ekzbfkezh fezhf lezf ekzjf ezfelzhf ezf ez fehf ez fieeklf elfj ekfjei fhelif efez fekf ef eif eek feskj fe fesr</p>
+@endsection
