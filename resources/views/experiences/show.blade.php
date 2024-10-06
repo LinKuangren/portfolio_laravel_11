0cec197c4f0beceef80fc0fc47c155da1cd59979
LinKuangren <romaindanne@gmail.com>
2024-10-06 02:30:19 +0200

    Ajout IsEmpty

--- resources/views/experiences/show.blade.php
+++ resources/views/experiences/show.blade.php
@@ -9,4 +9,17 @@
     <p>{{ $experience->company }}</p>
     <p>{{ $experience->time }}</p>
     <p>{{ $experience->content }}</p>
+    @if ($experience->categories->isEmpty())
+        <div>
+            <h3>Catégories :</h3>
+            <p>Aucune catégorie</p>
+        </div>
+    @else
+        <div>
+            <h3>Catégories :</h3>
+            @foreach ($experience->categories as $categorie)
+                <a class="hover:text-sky-600" href="{{ route('categories.showExperiences', ['name' => $categorie->name, 'categorie' => $categorie->id])}}">{{ $categorie->name }}</a>
+            @endforeach
+        </div>
+    @endif
 @endsection
--- resources/views/productions/show.blade.php
+++ resources/views/productions/show.blade.php
@@ -7,4 +7,17 @@
     <img class="h-32" src="{{ $production->imageUrl() }}" alt="{{ $production->image }}">
     <h1>{{ $production->title }}</h1>
     <p>{{ $production->content }}</p>
+    @if ($production->categories->isEmpty())
+        <div>
+            <h3>Catégories :</h3>
+            <p>Aucune catégorie</p>
+        </div>
+    @else
+        <div>
+            <h3>Catégories :</h3>
+            @foreach ($production->categories as $categorie)
+                <a class="hover:text-sky-600" href="{{ route('categories.showProductions', ['name' => $categorie->name, 'categorie' => $categorie->id])}}">{{ $categorie->name }}</a>
+            @endforeach
+        </div>
+    @endif
 @endsection
