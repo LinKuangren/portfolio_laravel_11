f813ab8972fb6ff3098351ecfb958c7baaf15aef
LinKuangren <romaindanne@gmail.com>
2025-02-12 19:34:32 +0100

    Update contact.blade.php

--- resources/views/main/contact.blade.php
+++ resources/views/main/contact.blade.php
@@ -14,7 +14,7 @@
         <x-honeypot />
         <div class="mb-5">
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-            <input type="text" name="email" value="{{ old('email') }}" class="bg-gray-100 border border-gray-500 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="azerty@gmail.com" required>
+            <input type="text" name="email" value="{{ old('email') }}" class="bg-gray-100 border border-gray-500 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="azerty@gmail.com" autofocus required>
             @error('email')
                 {{ $message }}
             @enderror
@@ -35,7 +35,7 @@
         </div>
         <div class="mb-5">
             <label for="content" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contenu</label>
-            <textarea name="content" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-600 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Contenu du mail" required>{{ old('content') }}</textarea>
+            <textarea rows="10" name="content" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-600 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Contenu du mail" required>{{ old('content') }}</textarea>
             @error('content')
                 {{ $message }}
             @enderror
